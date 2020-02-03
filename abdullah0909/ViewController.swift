567b212c7c24ba5baa5e02ef0b090b206f21d28e
Mona Lisa <abososo5656@hotmail.com>
2020-02-03 15:03:37 +0300

    initial

---
+++ abdullah0909/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  abdullah0909
+//
+//  Created by abdullah  on 27/05/1441 AH.
+//  Copyright © 1441 abdullah . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
